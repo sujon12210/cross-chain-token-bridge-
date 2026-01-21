c2ba5fef67a3228382b3e8d8012f3baa5671f4d6
Sujon <160887695+sujon12210@users.noreply.github.com>
2026-01-21 21:50:29 +0600

    Create networks.config.js

---
+++ networks.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    source: {
+        chainId: 11155111, // Sepolia
+        rpc: "https://eth-sepolia.g.alchemy.com/v2/KEY"
+    },
+    destination: {
+        chainId: 80001, // Mumbai
+        rpc: "https://polygon-mumbai.g.alchemy.com/v2/KEY"
+    }
+};
